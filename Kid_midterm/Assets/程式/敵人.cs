e70de3252670d52a99a5e696d551000a4c39b986
SleepEnough <91520050+SleepEnoughST@users.noreply.github.com>
2021-11-16 22:41:20 +0800

    2021/11/1602

--- Kid_midterm/Assets/程式/敵人.cs
+++ Kid_midterm/Assets/程式/敵人.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class 敵人 : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
     [SerializeField] private int damage;
     [SerializeField] private CircleCollider2D CircleCollider;
     [SerializeField] private LayerMask PlayerLayer;
+
     private float cooldownTimer = Mathf.Infinity;
+
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
@@ -18,14 +28,17 @@ public class 敵人 : MonoBehaviour
         {
             if (cooldownTimer >= attackCooldown)
             {
-
-            }    
+                cooldownTimer = 0;
+                anim.SetTrigger("觸發攻擊");
+            }
         }
     }
 
     private bool PlayerInSight()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(CircleCollider.bounds.center, CircleCollider.bounds.size, 0, Vector2.left, 0, PlayerLayer);
+        RaycastHit2D hit = Physics2D.BoxCast(CircleCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+        new Vector3(CircleCollider.bounds.size.x * range, CircleCollider.bounds.size.y, CircleCollider.bounds.size.z),
+        0, Vector2.left, 0, PlayerLayer);
 
         return hit.collider != null;
     }
@@ -33,6 +46,7 @@ public class 敵人 : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(CircleCollider.bounds.center, CircleCollider.bounds.size);
+        Gizmos.DrawWireCube(CircleCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(CircleCollider.bounds.size.x * range, CircleCollider.bounds.size.y, CircleCollider.bounds.size.z));
     }
 }
