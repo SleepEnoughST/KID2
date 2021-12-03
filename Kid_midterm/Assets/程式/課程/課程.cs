e0dcf6153ed84246147aa8ca8dad70fda24af40a
SleepEnoughST <108051085@live.asia.edu.tw>
2021-12-03 15:47:57 +0800

    2021/12/03

--- Kid_midterm/Assets/程式/課程/課程.cs
+++ Kid_midterm/Assets/程式/課程/課程.cs
@@ -15,9 +15,22 @@ public class 課程 : MonoBehaviour
     [Header("跳躍按鍵與可跳躍圖層")]
     public KeyCode keyjump = KeyCode.Space;
     public LayerMask canJumpLayer;
-    
-    private Rigidbody2D rb2D;
+    public string parameterWalk = "角色走路";
+    public string parameterJump = "角色跳躍";
+    #endregion
 
+    #region 欄位:私人
+    private Rigidbody2D rb2D;
+    private Animator anim;
+    [SerializeField]
+    private bool IsGround;
+    [SerializeField]
+    private int jumpCount;
+    [SerializeField]
+    private bool jumpPress;
+    #endregion
+
+    #region 事件
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color(1, 0, 20, 0.5f);
@@ -27,6 +40,7 @@ public class 課程 : MonoBehaviour
     private void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -36,20 +50,26 @@ public class 課程 : MonoBehaviour
     private void Update()
     {
         Flip();
+        CheckGround();
+        Jump();
     }
+    #endregion
 
+    #region 方法
     public void Move()
     {
         float M = Input.GetAxis("Horizontal");
-        print("玩家左右按鍵值:" + M);
+        //print("玩家左右按鍵值:" + M);
 
         rb2D.velocity = new Vector2(M * speed, rb2D.velocity.y);
+
+        anim.SetBool(parameterWalk, M != 0);
     }
     private void Flip()
     {
         float M = Input.GetAxis("Horizontal");
 
-        if(M < 0)
+        if (M < 0)
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
@@ -59,5 +79,31 @@ public class 課程 : MonoBehaviour
         }
     }
 
+    private void CheckGround()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position + transform.TransformDirection(checkGroundOffset), checkGroundRadius, canJumpLayer);
+        //print("碰到的物件:" + hit.name);
+        IsGround = hit;
+
+        anim.SetBool(parameterJump, !IsGround);
+    }
+
+    private void Jump()
+    {
+        
+        if (IsGround)
+        {
+            jumpCount = 0;
+
+            if (Input.GetKeyDown(keyjump) && jumpCount < 2)
+            {
+                rb2D.AddForce(new Vector2(0, jump));
+                jumpCount++;
+                jumpPress = true;
+            }
+
+            
+        }
+    }
     #endregion
 }
