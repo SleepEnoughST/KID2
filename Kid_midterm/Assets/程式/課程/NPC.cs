57ba1bbf4cb71e48bd15c157874a08173369d247
SleepEnoughST <108051085@live.asia.edu.tw>
2021-12-17 15:19:26 +0800

    2021/12/17_1

---
+++ Kid_midterm/Assets/程式/課程/NPC.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("對話資料")]
+    public 對話 dataDialogue;
+    [Header("對話系統")]
+    public 對話系統 dialogueSystem;
+    [Header("觸發對話的對象")]
+    public string target = "小紅帽";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == target)
+        {
+            print(collision.name);
+            print("叮咚！");
+        }
+    }
+}
